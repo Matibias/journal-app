14a2d987600135103fbf7c8a6125ffb00b117769
Matias Parra <mparrab103@gmail.com>
2023-04-09 19:10:41 -0400

    refactor: new views

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,10 +1,16 @@
 import { Typography } from '@mui/material'
-import React from 'react'
+import { JournalLayout } from '../layout/JournalLayout'
+import { NoteView, NothingSelectedView } from '../views'
 
 export const JournalPage = () => {
   return (
-    <>
-    <Typography variant='h1'>JournalPage</Typography>
-    </>
+    <JournalLayout>
+
+      {/* <Typography>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, quidem. Molestias nisi corporis explicabo? Non, minima cupiditate quis id, incidunt nihil corrupti odio nulla accusantium, quaerat nemo aspernatur voluptate dolores.</Typography> */}
+   
+      {/* <NothingSelectedView /> */}
+      <NoteView />
+
+    </ JournalLayout>
   )
 }
