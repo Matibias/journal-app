b911b14f298b04ee5ca4fa720b0275717b4ca0c5
Matibias <mparrab103@gmail.com>
2023-04-16 23:45:58 -0400

    add: thunks

--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const authSlice = createSlice({
     name: 'auth',
     initialState: {
-        status: 'checking', // 'checking' | 'authenticated' | 'not-authenticated'
+        status: 'not-authenticated', // 'checking' | 'authenticated' | 'not-authenticated'
         uid: null,
         email: null,
         displayName: null,
@@ -18,7 +18,7 @@ export const authSlice = createSlice({
 
         },
         checkingCredentials: (state) => {
-
+          state.status = 'checking';
         }
     }
 });
